fe2ab559e75a48c840d1b432c8172a47f3a81737
Leandro Perego <leperego@hotmail.com>
2022-07-08 14:47:52 -0300

    criado metodo para carregar a pagina sobre

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,14 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
 import gui.util.Alerta;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -33,11 +39,29 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemSobreAction() {
-		Alerta.showAlerta("Sobre", "Sobre nós", "Aplicacao com JavaFX demo", AlertType.INFORMATION);
+		loadView("/gui/Sobre.fxml");
 	}
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
+	}
+	
+	private synchronized void loadView(String caminho) {
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(caminho));
+			VBox newVBox = loader.load();
+			Scene mainScene = Main.getMainScene();
+			
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		} catch (IOException e) {
+			Alerta.showAlerta("IOException", "Erro ao carregar página", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
